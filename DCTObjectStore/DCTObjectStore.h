598f2801f0e3d4dd22155e976075d22858a47691
Daniel Tull <dt@danieltull.co.uk>
2014-01-13 08:47:20 +0000

    Initial Commit

---
+++ DCTObjectStore/DCTObjectStore.h
@@ -0,0 +1,13 @@
+//
+//  DCTObjectStore.h
+//  DCTObjectStore
+//
+//  Created by Daniel Tull on 13.01.2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DCTObjectStore : NSObject
+
+@end
---
+++ DCTObjectStore/DCTObjectStore.m
@@ -0,0 +1,13 @@
+//
+//  DCTObjectStore.m
+//  DCTObjectStore
+//
+//  Created by Daniel Tull on 13.01.2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+#import "DCTObjectStore.h"
+
+@implementation DCTObjectStore
+
+@end
