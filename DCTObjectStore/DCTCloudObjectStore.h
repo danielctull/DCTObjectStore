9f0e97cbd146d4ba3777a89cf4b29f226c0a2cca
Daniel Tull <dt@danieltull.co.uk>
2014-11-29 22:11:18 +0000

    Rename to DCTCloudObjectStore

---
+++ DCTObjectStore/DCTCloudObjectStore.h
@@ -0,0 +1,13 @@
+//
+//  DCTCloudObjectStore.h
+//  DCTObjectStore
+//
+//  Created by Daniel Tull on 29.11.2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DCTCloudObjectStore : NSObject
+
+@end
---
+++ DCTObjectStore/DCTCloudObjectStore.m
@@ -0,0 +1,13 @@
+//
+//  DCTCloudObjectStore.m
+//  DCTObjectStore
+//
+//  Created by Daniel Tull on 29.11.2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+#import "DCTCloudObjectStore.h"
+
+@implementation DCTCloudObjectStore
+
+@end
